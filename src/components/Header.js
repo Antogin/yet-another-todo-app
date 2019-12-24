d5aae19378fb019e9264a73b16e3966016a6d4b6
Antogin <georges.indrianjafy@gmail.com>
2019-12-24 15:08:18 +0100

    type

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
 	return (
 		<nav className="navbar" role="navigation" aria-label="main navigation">
 			<div className="navbar-brand">
-				<div className="navbar-item">Yet another todo ap</div>
+				<div className="navbar-item">Yet Another TODO App</div>
 			</div>
 		</nav>
 	);
