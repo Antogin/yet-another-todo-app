9f145ed028b5b8432a579941169ac79c596e8e23
Antogin <georges.indrianjafy@gmail.com>
2019-12-25 13:44:33 +0100

    clean up

--- src/context/TodoContext.js
+++ src/context/TodoContext.js
@@ -3,9 +3,21 @@ import uuid from 'uuid/v1';
 
 export const TodoContext = createContext();
 
-const TodoContextProvider = ({ children }) => {
-	const [ todoItems, setTodoItems ] = useState(JSON.parse(localStorage.getItem('todos')) || []);
+const defaultTodo = [
+	{ title: 'Learn about hooks', id: '5dc99cd0-2712-11ea-aeb4-9f34813c101c', done: true },
+	{ title: 'Learn about context', id: '62a55b40-2712-11ea-aeb4-9f34813c101c', done: true },
+	{ title: 'Deploy the thing', id: '660e9df0-2712-11ea-aeb4-9f34813c101c', done: true },
+	{ title: 'PWA support', id: '684c0ad0-2712-11ea-aeb4-9f34813c101c', done: true },
+	{ title: 'Auth', id: '6b025130-2712-11ea-aeb4-9f34813c101c', done: false },
+	{ title: 'Online sync', id: '6dad4cf0-2712-11ea-aeb4-9f34813c101c', done: false },
+	{ title: 'Real time updates', id: '71864160-2712-11ea-aeb4-9f34813c101c', done: false }
+];
 
+const TodoContextProvider = ({ children }) => {
+	const todos = JSON.parse(localStorage.getItem('todos'));
+	const [ todoItems, setTodoItems ] = useState(todos || defaultTodo);
+
+	console.log(JSON.parse(localStorage.getItem('todos')));
 	const updateTodoItems = (todos) => {
 		setTodoItems(todos);
 		localStorage.setItem('todos', JSON.stringify(todos));
