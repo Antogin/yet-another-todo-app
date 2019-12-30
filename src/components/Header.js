0b1ac8405bca9a657349b588bf17a4dafe7e2211
Antogin <georges.indrianjafy@gmail.com>
2019-12-30 11:26:53 +0100

    remove api calls

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from 'react';
 import './Header.scss';
 
 const Header = () => {
-	const [ message, setMessage ] = useState('');
-
-	useEffect(() => {
-		axios.get('.netlify/functions/api/').then(({ data: { message } }) => {
-			setMessage(message);
-		});
-	}, []);
 	return (
 		<nav className="navbar" role="navigation" aria-label="main navigation">
 			<div className="navbar-brand">
 				<img src="task-list.svg" className="app-logo" alt="logo" />
 				<div className="navbar-item">Yet Another TODO App</div>
-				<div>{message}</div>
 			</div>
 		</nav>
 	);
