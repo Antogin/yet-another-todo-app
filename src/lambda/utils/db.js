cac683f505cabda33ebf831d64f0473ddde12168
Antogin <georges.indrianjafy@gmail.com>
2019-12-30 11:31:46 +0100

    remove functions

--- src/lambda/utils/db.js
+++
@@ -1,9 +0,0 @@
-import { connect } from 'mongoose';
-
-const dbUrl = process.env.DB_HOST;
-const dbUser = process.env.DB_USER;
-const dbPass = process.env.DB_PWD;
-
-connect(`mongodb+srv://${dbUser}:${dbPass}@${dbUrl}/test?retryWrites=true&w=majority`, {
-	useNewUrlParser: true
-});
